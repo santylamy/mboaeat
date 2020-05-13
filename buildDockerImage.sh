907226f7e7626814e8ce2a6f916e07d57842a7f7
santylamy <moussi.salomon@gmail.com>
2020-05-13 16:12:05 +0200

    order service

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-modules=( account-api customer-api gateway-service)
+modules=( account-api customer-api gateway-service order-api)
 
 for module in "${modules[@]}"; do
     docker build -t "mboaeat/${module}:latest" ${module}
